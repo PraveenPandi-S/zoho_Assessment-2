b6958e76e1c694afdb0f6078582ab5cdcb137c6f
Praveen <115635810+PraveenPandi-S@users.noreply.github.com>
2023-12-02 14:17:13 +0530

    Add files via upload

---
+++ Output.java
@@ -0,0 +1,16 @@
+package Todaytest;
+public class Output
+{
+	public static void main(String[] args) {
+		for (int i=45;i>38;i--){
+		    if(i == 43){
+		        System.out.println();
+		        continue;
+		    }
+		    System.out.print(i);		}
+	}
+}
+
+output:
+4544
+42414039
